edd4f715589d28b389b9a582987111df07deb980
HarshDagar <harshdagar8181@gmail.com>
2026-06-08 04:49:30 +0530

    add enable mdb repositories in main entrypoint class

--- src/main/java/com/wallet/digital_wallet/DigitalWalletApplication.java
+++ src/main/java/com/wallet/digital_wallet/DigitalWalletApplication.java
@@ -2,12 +2,12 @@ package com.wallet.digital_wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class DigitalWalletApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DigitalWalletApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalWalletApplication.class, args);
+    }
 }
