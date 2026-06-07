218e214693a9d93c9865ff224f59a966fd72462d
HarshDagar <harshdagar8181@gmail.com>
2026-06-08 03:36:41 +0530

    enable mdb auditing, auto fill some fields while uploading mdb docs

---
+++ src/main/java/com/wallet/digital_wallet/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.wallet.digital_wallet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
